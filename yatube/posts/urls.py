25cf648c8feab14d0172f177fd2112e34bb45cae
Alina Arslanova <ararslanova@mail.ru>
2022-04-20 21:04:04 +0500

    Completed project

---
+++ yatube/posts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    # Главная страница:
+    # если получен запрос без относительного адреса
+    # (то есть это запрос к имени домена, например anfisa.com),
+    # будет вызвана функция index() из файла views.py
+    path('', views.index, name='index'),
+    # Страница со списком групп
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
+]

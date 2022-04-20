88df709a0b935aad01c29d6bc0179b41e7000e3f
Alina Arslanova <ararslanova@mail.ru>
2022-04-20 21:19:38 +0500

    Completed project

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-#from django.http import HttpResponse
-from .models import Post, Group 
+from .models import Post, Group
 
 
 def index(request):
